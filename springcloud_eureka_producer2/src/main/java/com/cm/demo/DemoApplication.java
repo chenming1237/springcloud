2cccf3254e81928355f24105e23140a5e9b2cba1
chenming1237 <464598056@qq.com>
2019-01-27 18:31:42 +0800

    1111

--- springcloud_eureka_producer2/src/main/java/com/cm/demo/DemoApplication.java
+++ springcloud_eureka_producer2/src/main/java/com/cm/demo/DemoApplication.java
@@ -22,7 +22,7 @@ public class DemoApplication {
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+        return "hi " + name + " ,i am from port1:" + port;
     }
 
 }
